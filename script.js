1daf4aec6d449e8102e1eddc8a0abc9099ee9ac7
freddy <albertcn12@gmail.com>
2021-08-05 20:39:14 -0500

    add feature to configure multiple slides

--- script.js
+++ script.js
@@ -1,41 +1,68 @@
-let slidesPerPage = 1;
-let margin = -100;// / slidesPerPage;
-let minHeight = '300px';
-let maxHeight = '300px';
+const slidesPerPage = 1;
+const margin = -100 / slidesPerPage;
+const minHeight = '300px';
+const maxHeight = '300px';
+const heightAdjustment = 'auto'; // or auto 
 let currentSlide = 0;
-let autoHeight = true;
-let slideTrack = document.querySelector('.inner-frame'); 
-let divs = document.querySelectorAll('.slide');
-let sections = Math.floor(divs.length / slidesPerPage);
-sections = (divs.length % slidesPerPage) > 0 ? sections + 1 : sections;
 
+const dotsContainer = document.getElementsByClassName('slide-dots-container')[0];
+const slideTrack = document.querySelector('.inner-frame'); 
+const slides = document.querySelectorAll('.slide');
+
+let sections = Math.floor(slides.length / slidesPerPage);
+sections = (slides.length % slidesPerPage) > 0 ? sections + 1 : sections;
+
+generateSlideDots();
 next();
-//generateSlideDots();
 
 function previous(){
-    currentSlide = currentSlide <= 1 ? divs.length : currentSlide -1;
+    currentSlide = currentSlide <= 1 ? sections: currentSlide -1;
     toggleSlides(currentSlide);
 }
 
 function next(){
-    currentSlide = currentSlide >= divs.length ? 1 : currentSlide +1;
+    currentSlide = currentSlide >= sections ? 1 : currentSlide +1;
     toggleSlides(currentSlide);
 }
 
 function toggleSlides(current){
-    slideTrack.style.marginLeft = `${(current-1) * margin}%`;
-    divs.forEach((div, index) => {
-        div.style.maxHeight = maxHeight;//(index+1) !== current ? `${minHeight}` : `${maxHeight}`;
-        //div.style.opacity = (index+1) !== current ? '0' : '1';
-        div.style.minWidth = `${100/slidesPerPage}%`;//(index+1) !== current ? '0%' : '100%';
+    currentSlide = current
+    let currentSet = [];
+
+    for(let n = 0; n < slidesPerPage; n++){
+        const item = (current-1) * slidesPerPage + n;
+        if(item < slides.length){
+            currentSet.push(item)
+        }
+    }
+    const marginToLeft = `${(current-1) * margin * slidesPerPage - (margin * (slidesPerPage - currentSet.length))}%`;
+    slideTrack.style.marginLeft = marginToLeft;
+
+    Array.from(dotsContainer.children).forEach((dot, index) => {
+        if(index == current -1){
+            dot.classList.add('active')
+        }else{
+            dot.classList.remove('active')
+        }
+    });
+
+    slides.forEach((div, index) => {
+        div.style.minWidth = `${100/slidesPerPage}%`;
+
+        if(heightAdjustment === 'auto'){
+            div.style.minHeight = (index+1) !== current*slidesPerPage? '0px' : '0px';
+            div.style.maxHeight = currentSet.includes(index) ? '3000px' : '0px';
+        }else if(heightAdjustment === 'fixed'){
+            div.style.minHeight = minHeight;
+            div.style.maxHeight = maxHeight;
+            div.style.overflowY = 'auto';
+        }
     });
 }
-/*      
+
 function generateSlideDots(){
-    let dotsContainer = document.getElementsByClassName('slide-dots-container')[0];
     for(let i = 0; i < sections; i++){
         const button = `<button class="slide-dot" onclick="toggleSlides(${i+1})"></button>`
         dotsContainer.innerHTML += button;
     }
 }
-*/
